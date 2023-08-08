d6beb51ec2a1822737b3bc47ab4ad52b2db1fd66
Liliane Souza <liliane.a.cruz.souza@gmail.com>
2023-08-07 23:13:11 -0300

    Initial commit

---
+++ Exercicio1/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Valor;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Valor * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            if (quantidade >= Quantidade)
+                Quantidade = 0;
+            else
+                Quantidade -= quantidade;
+        }
+
+        public void MostrarProduto()
+        {
+            Console.Clear();
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Valor: {Valor:C}");
+            Console.WriteLine($"Quantidade: {Quantidade}");
+            Console.WriteLine($"Valor Total em Estoque: {ValorTotalEmEstoque():C}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto mercadoria = new Produto();
+            mercadoria.Nome = "Mouse";
+            mercadoria.Valor = 250.89;
+            mercadoria.Quantidade = 5;
+
+            mercadoria.MostrarProduto();
+            mercadoria.AdicionarProduto(3);
+            mercadoria.MostrarProduto();
+            mercadoria.RemoverProdutos(2);
+            mercadoria.MostrarProduto();
+        }
+    }
+}
